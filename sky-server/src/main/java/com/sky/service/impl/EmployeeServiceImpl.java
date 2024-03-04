9b78b2d6bc84f08b6c008a2697c4baa40b9c86a2
TSH <hello@itcast.cn>
2024-03-04 11:13:01 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -96,4 +96,17 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(employees.getTotal(),employees.getResult());
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    @Override
+    public void enableordisable(Integer status, Long id) {
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setStatus(status);
+        employeeMapper.update(employee);
+    }
+
 }
