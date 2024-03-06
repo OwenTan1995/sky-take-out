f9f0e851731815661cf2d810919b6e7bf4d440d9
TSH <hello@itcast.cn>
2024-03-06 09:27:22 +0800

    文件上传

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,18 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OssConfiguration {
+    @Bean
+    public AliOssUtil aliossUtil(AliOssProperties aliOssProperties){
+        AliOssUtil aliOssUtil = new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+        return aliOssUtil;
+    }
+}
